3e2849aa12afe8205db3bcecfc195ff2c7be9883
Alan <shoujunxie@gmail.com>
2021-07-30 19:13:56 +0800

    ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Platform, requireNativeComponent, StyleProp, ViewStyle } from 'react-native'
 
 export type GooglePayImageProps = {
-  type?: 'buy' | 'plain'
+  type?: 'buy' | 'plain' | 'donate'
   style?: StyleProp<ViewStyle>
 }
 
 const BuyWithGooglePayImageViewManager = requireNativeComponent<GooglePayImageProps>('BuyWithGooglePayImageView')
+const DonateWithGooglePayImageViewManager = requireNativeComponent<GooglePayImageProps>('DonateWithGooglePayImageView')
 const PlainGooglePayImageViewManager = requireNativeComponent<GooglePayImageProps>('PlainGooglePayImageView')
 
 export const GooglePayImageView: React.FC<GooglePayImageProps> = ({ type, ...props }) => {
@@ -16,6 +17,10 @@ export const GooglePayImageView: React.FC<GooglePayImageProps> = ({ type, ...pro
 
   if (type === 'buy') {
     return <BuyWithGooglePayImageViewManager {...props} />
+  }
+
+  if (type === 'donate') {
+    return <DonateWithGooglePayImageViewManager {...props} />
   }
 
   return <PlainGooglePayImageViewManager {...props} />
